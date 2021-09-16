2ebd23b281aac7905e51d53fee2a3f5dc7ef9951
Your Name <you@example.com>
2021-09-16 14:44:00 +0300

    3 version of project

--- src/sptv20gitpro/SPTV20GitPro.java
+++ src/sptv20gitpro/SPTV20GitPro.java
@@ -17,6 +17,7 @@ public class SPTV20GitPro {
     public static void main(String[] args) {
         System.out.println("1 v of project");
         System.out.println("2 v of project");
+        System.out.println("3 v of project");
     }
     
 }
